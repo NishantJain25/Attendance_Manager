a0d0a9c45bc1fc4d144c25946c9c484b5d381785
Nishant <nishantjain2503@gmail.com>
2022-09-13 23:54:37 +0530

    feat: Added dynamic subject list with new subject form

--- attendance-manager/src/App.js
+++ attendance-manager/src/App.js
@@ -1,27 +1,46 @@
-import './App.scss'
-import {SubjectCard} from './components/SubjectCard.js'
+import "./App.scss"
+import React, { useState } from "react"
+import { SubjectCard } from "./components/SubjectCard.js"
+import {Form} from "./components/Form.js"
 
 function App() {
-  return (
-    <div className="App">
-      <header><h1>Attendance Manager</h1></header>
-      <div id = "details">
-        <div id = "attendance-criteria" className = "overall-details">
-          <p>Goal</p>
-          <h2>75%</h2>
-        </div>
-        <div id = "overall-attendance" className = "overall-details">
-          <p>Overall Attendance</p>
-          <h2>66%</h2>
-        </div>
-        <div id = "attendance-circle">66%</div>
-        <button id = "add-subject">Add subject</button>
-      </div>
-      <div id = "container">
-          <SubjectCard />
-      </div>
-    </div>
-  );
+	const [subjectList, setSubjectList] = useState([])
+	
+	const [hideForm, setHideForm] = useState(true)
+
+	
+	return (
+		<div className="App">
+			<header>
+				<h1>Attendance Manager</h1>
+			</header>
+			<div id="details">
+				<div id="attendance-criteria" className="overall-details">
+					<p>Goal</p>
+					<h2>75%</h2>
+				</div>
+				<div id="overall-attendance" className="overall-details">
+					<p>Overall Attendance</p>
+					<h2>66%</h2>
+				</div>
+				<div id="attendance-circle">66%</div>
+				<button
+					id="add-subject"
+					onClick={() => setHideForm((prevState) => !prevState)}
+				>
+					{hideForm ? 'Add subject' : 'Close'}
+				</button>
+			</div>
+			<Form hideForm = {hideForm} setHideForm = {setHideForm} setSubjectList = {setSubjectList} />
+			<div id="container">
+				{subjectList.length > 0 ? (
+					subjectList.map((subject,key) => <SubjectCard subject = {subject} key = {key}/>)
+				) : (
+					<h2>You haven't added any Subjects yet</h2>
+				)}
+			</div>
+		</div>
+	)
 }
 
-export default App;
+export default App
